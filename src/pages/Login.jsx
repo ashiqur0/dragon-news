c5ef6c2aceed36061bdcf6d7254b645ddeff5f72
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-10-24 09:06:46 +0600

    Register Page Added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,19 +6,23 @@ const Login = () => {
         <div className='flex justify-center mt-20'>
             <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
 
-                <h2 className='font-semibold text-4xl text-center pt-10 pb-5 text-primary'>Login your account</h2>
+                <h2 className='font-semibold text-3xl text-center pt-10 pb-5 text-primary'>Login your account</h2>
 
                 <div className="card-body">
                     <fieldset className="fieldset">
+
+                        {/* Email Field */}
                         <label className="label">Email</label>
                         <input type="email" className="input" placeholder="Email" />
+
+                        {/* Password Field */}
                         <label className="label">Password</label>
                         <input type="password" className="input" placeholder="Password" />
                         <div><a className="link link-hover">Forgot password?</a></div>
                         <button className="btn btn-neutral mt-4">Login</button>
                     </fieldset>
 
-                    <p className='text-center font-semibold text-[0.875rem] text-primary mt-3'>Dont’t Have An Account ? <span className='text-red-500'><Link to='/auth/register'>Register</Link></span></p>
+                    <p className='text-center font-semibold text-[0.875rem] text-primary mt-3'>Dont’t Have An Account ? <Link to='/auth/register' className='text-red-500 hover:underline'>Register</Link></p>
                 </div>
             </div>
         </div>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Register = () => {
     return (
-        <div>
-            Register pages
+        <div className='flex justify-center mt-20'>
+            <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+
+                <h2 className='font-semibold text-3xl text-center pt-10 pb-5 text-primary'>Register your account</h2>
+
+                <div className="card-body">
+                    <fieldset className="fieldset">
+
+                        {/* Name Field */}
+                        <label className="label">Name</label>
+                        <input type="text" className="input" placeholder="Name" />
+
+                        {/* Photo Url Field */}
+                        <label className="label">Photo Url</label>
+                        <input type="text" className="input" placeholder="Photo Url" />
+
+                        {/* Email Field */}
+                        <label className="label">Email</label>
+                        <input type="email" className="input" placeholder="Email" />
+
+                        {/* Password Field */}
+                        <label className="label">Password</label>
+                        <input type="password" className="input" placeholder="Password" />
+                        
+                        {/* Login Button */}
+                        <button className="btn btn-neutral mt-4">Login</button>
+                    </fieldset>
+
+                    <p className='text-center font-semibold text-[0.875rem] text-primary mt-3'>Already have an account ? <Link to='/auth/login' className='text-green-500 hover:underline'>Login</Link></p>
+                </div>
+            </div>
         </div>
     );
 };
